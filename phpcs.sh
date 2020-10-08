6199a793d189ab738f19d85ad1c237de524c4052
Leon Francis Shelhamer <leon@211j.com>
2020-10-08 19:53:57 -0400

    bash files
    Signed-off-by: Leon Francis Shelhamer <leon@211j.com>

---
+++ phpcs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ ${RUN_PHPCS} == 1 ]]; then
+	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
+	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+
+	if [ "$CHANGED_FILES" != "" ]; then
+		if [ ! -f "./vendor/bin/phpcs" ]; then
+			# Install wpcs globally
+			composer global require woocommerce/woocommerce-sniffs --update-with-all-dependencies
+		fi
+
+		echo "Running Code Sniffer."
+		phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p $CHANGED_FILES
+	fi
+fi
